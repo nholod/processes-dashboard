4782e355933e4bb929dc398ad32b05a9fb86e2f7
nholod <nholod@gmail.com>
2026-09-07 20:20:19 +0300

    Перенести daily-note-questions в личностное развитие

--- generate_dashboard.py
+++ generate_dashboard.py
@@ -206,6 +206,7 @@ YANA_IDS = {
 }
 
 PERSONAL_IDS = {
+    "9484101d-b301-4440-975d-6c9746e87bbe",
     "c2f52127-3d5c-4bf5-901b-394831f8f4e9",
     "379c1d61-e15b-4e3f-b3e0-fdede841c113",
     "1682e60e-23cc-4b46-968f-ba4823dc8dff",
